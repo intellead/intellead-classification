bf0aa6bf6202b970fbd5b672b74e54521b37e0a6
Vinícius Gomes <vinigomes@Viniciuss-MacBook-Pro.local>
2017-07-28 12:26:24 -0700

    test

--- service.py
+++ service.py
@@ -11,20 +11,15 @@ s3 = S3Connection(os.environ['DATABASE_NAME'], os.environ['DATABASE_USER'], os.e
 
 def classification(data_from_lead):
     dataset = get_dataset_from_database()
-
-
+    x = get_dataset_input_from_database()
+    y = get_dataset_output_from_database()
     # x são as entradas e y são as saídas
     #dataset1
     #x = np.genfromtxt('dataset.csv', delimiter=';', usecols=(3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21))
     #y = np.genfromtxt('dataset.csv', delimiter=';', usecols=(22))
     #dataset2
-
-    print('TEST')
-    print(type(dataset))
-    print(type(dataset[0]))
-
-    x = np.genfromtxt('dataset.csv', delimiter=';', usecols=(3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13))
-    y = np.genfromtxt('dataset.csv', delimiter=';', usecols=(14))
+    #x = np.genfromtxt('dataset.csv', delimiter=';', usecols=(3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13))
+    #y = np.genfromtxt('dataset.csv', delimiter=';', usecols=(14))
     print('The total number of examples in the dataset is: %d' % (len(x)))
     x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size=0.3, random_state=42)
     print('The number of examples used for training are: %d' % (len(x_treino)))
@@ -44,7 +39,7 @@ def get_dataset_from_database():
     rows = [];
     try:
         print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(host=os.environ.get('DATABASE_HOST'), database=os.environ.get('DATABASE_NAME'), user=os.environ.get('DATABASE_USER'), password=os.environ.get('DATABASE_PASSWORD'))
+        conn = get_connection()
         cur = conn.cursor()
         cur.execute('SELECT * FROM dataset')
         rows = cur.fetchall()
@@ -57,3 +52,46 @@ def get_dataset_from_database():
             conn.close()
             print('Database connection closed.')
             return rows
+
+
+def get_dataset_input_from_database():
+    rows = [];
+    try:
+        conn = get_connection()
+        cur = conn.cursor()
+        cur.execute('SELECT company, email, name, job_title, lead_profile, conversions, area, number_employees, segment, work_in_progress, source_first_conversion, source_last_conversion, concern, looking_for_management_software FROM dataset')
+        rows = cur.fetchall()
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            return np.array(rows)
+
+
+def get_dataset_output_from_database():
+    rows = [];
+    try:
+        conn = get_connection()
+        cur = conn.cursor()
+        cur.execute('SELECT lead_status FROM dataset')
+        rows = cur.fetchall()
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            return np.array(rows)
+
+
+def get_connection():
+    try:
+        conn = psycopg2.connect(host=os.environ.get('DATABASE_HOST'), database=os.environ.get('DATABASE_NAME'),
+                                user=os.environ.get('DATABASE_USER'), password=os.environ.get('DATABASE_PASSWORD'))
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            return conn
