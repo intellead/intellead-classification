1153c53ea433b9b7c628df82daae72a6802ffe3b
Vinícius Gomes <vinigomes@viniciuss-mbp.softplan.com.br>
2017-08-25 09:14:07 -0300

    test

--- app.py
+++ app.py
@@ -56,8 +56,9 @@ def save_lead_status(lead_id, lead_status):
 
 
 def send_data_to_connector(json_lead):
-    leads = {[json_lead['lead']]}
+    leads = {'leads': {[json_lead['lead']]}}
     print('connecting... to intellead-connector')
+    print(leads)
     headers = {
         'content-type': 'application/json'
     }
