3a38cc0b5f9ebb2fdf0c892f34598c2fc587c213
Vinícius Gomes <vinigomes@viniciuss-mbp.softplan.com.br>
2017-08-25 16:14:42 -0300

    test

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def send_data_to_connector(json_lead):
         'Cache-Control': 'no-cache'
     }
     url = 'https://intellead-connector.herokuapp.com/teste'
-    r = requests.post(url, data=leads, headers=headers)
+    r = requests.post(url, data=json.dumps(leads), headers=headers)
     print(r.status_code)
 
 
