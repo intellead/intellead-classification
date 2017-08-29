400ef2b86b19c72908382845c559be28b35e4591
Vinícius Gomes <vinigomes@viniciuss-mbp.softplan.com.br>
2017-08-29 09:53:03 -0300

    turn off

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def get_lead_status_by_id(lead_id):
     save_lead_status(lead_id, lead_status)
     if lead_status == 1:
         json_lead['lead']['lead_status'] = int(lead_status)
-        send_data_to_connector(json_lead['lead'])
+        #send_data_to_connector(json_lead['lead'])
         print('connected')
     return str(lead_status)
 
