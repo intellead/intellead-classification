b5d4ce8a580ac096994a1d29c27c54b4d7b9481e
gniehues <gniehues90@gmail.com>
2018-04-18 10:02:13 -0300

    main activity removed from code

--- service.py
+++ service.py
@@ -31,7 +31,7 @@ def classification(customer, lead):
     #    ('dt', DecisionTreeClassifier(max_depth=5)),
     #    ('kn', KNeighborsClassifier(16)),
     #]
-    inputs = get_dataset_input_from_database(customer, lead.keys())
+    inputs = get_dataset_input_from_database(customer)
     outputs = get_dataset_output_from_database(customer)
     print('The total number of examples in the dataset is: %d' % (len(inputs)))
     inputs_training, inputs_test, outputs_training, outputs_test = train_test_split(inputs, outputs, test_size=0.3, random_state=42)
@@ -63,17 +63,12 @@ def classification(customer, lead):
 
 
 def convert_dict_to_tuple(data):
-    data_tuple = (data['role'], data['profile'], data['conversion'], data['lead_area'], data['number_of_employees'], data['company_segment'], data['wip'], data['source_first_conv'], data['source_last_conv'], data['concern'], data['looking_for_a_software'])
-    if 'main_activity' in data.keys():
-        data_tuple = data_tuple + (data['main_activity'],)
-    return data_tuple
+    return (data['role'], data['profile'], data['conversion'], data['lead_area'], data['number_of_employees'], data['company_segment'], data['wip'], data['source_first_conv'], data['source_last_conv'], data['concern'], data['looking_for_a_software'], data['main_activity'])
 
 
 def save_lead_in_dataset(data):
     try:
-        columns = ['email', 'job_title', 'lead_profile', 'conversions', 'area', 'number_employees', 'segment', 'work_in_progress', 'source_first_conversion', 'source_last_conversion', 'concern', 'looking_for_management_software']
-        if 'main_activity' in data.keys():
-            columns.append('cnae')
+        columns = ['email', 'job_title', 'lead_profile', 'conversions', 'area', 'number_employees', 'segment', 'work_in_progress', 'source_first_conversion', 'source_last_conversion', 'concern', 'looking_for_management_software', 'cnae']
         values = ((data['email'],) + convert_dict_to_tuple(data))
         insert_statement = 'insert into dataset (%s) values %s'
         conn = get_connection()
@@ -88,15 +83,12 @@ def save_lead_in_dataset(data):
             conn.close()
 
 
-def get_dataset_input_from_database(customer, fields):
+def get_dataset_input_from_database(customer):
     rows = [];
     try:
         conn = get_connection()
         cur = conn.cursor()
-        if 'main_activity' in fields:
-            cur.execute('SELECT job_title, lead_profile, conversions, area, number_employees, segment, work_in_progress, source_first_conversion, source_last_conversion, concern, looking_for_management_software, cnae FROM dataset WHERE customer = %s', [customer])
-        else:
-            cur.execute('SELECT job_title, lead_profile, conversions, area, number_employees, segment, work_in_progress, source_first_conversion, source_last_conversion, concern, looking_for_management_software FROM dataset WHERE customer = %s', [customer])
+        cur.execute('SELECT job_title, lead_profile, conversions, area, number_employees, segment, work_in_progress, source_first_conversion, source_last_conversion, concern, looking_for_management_software, cnae FROM dataset WHERE customer = %s', [customer])
         rows = cur.fetchall()
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
